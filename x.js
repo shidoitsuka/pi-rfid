d95dd93877004b8e09bf14123d41bb34fbe3c1bf
shidoitsuka <mail@standinshd.site>
2020-02-12 07:19:32 +0700

    removed unnecessary stuffs

--- x.js
+++
@@ -1,7 +0,0 @@
-const {PythonShell} = require("python-shell");
-const py = new PythonShell('x.py');
-py.send(JSON.stringify([1,2,3]))
-py.on('message', function(m) {
-console.log(m);
-});
-py.end(e=> {if (e) throw e;  console.log('e')})
--- x.py
+++
@@ -1,8 +0,0 @@
-import sys, json
-
-def read_in():
-	lines = sys.stdin.readlines()
-	return json.loads(lines[0])
-	
-lines = read_in()
-print(lines)
