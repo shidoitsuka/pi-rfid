7506295c873fd135a5692034487e3e4f8078e8b5
Brian <mail@standinshd.site>
2020-02-15 18:19:45 -0800

    beautified

--- index.js
+++ index.js
@@ -2,48 +2,44 @@ const express = require("express");
 const hbs = require("hbs");
 const bodyParser = require("body-parser");
 const path = require("path");
-const {
-    PythonShell
-} = require("python-shell");
+const { PythonShell } = require("python-shell");
 const app = express();
 
 app.set("views", path.join(__dirname, "src/views"));
 app.set("view engine", "hbs");
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({
-    extended: false
-}));
+app.use(bodyParser.urlencoded({ extended: false }));
 
 /* user open the page */
 // index
 app.get("/", (req, res) => {
-    res.render(`index/index`);
+  res.render(`index/index`);
 });
 
 // login form
 app.get("/login", (req, res) => {
-    res.render(`login/index`);
+  res.render(`login/index`);
 });
 
 // seller's form
 app.get("/buy", (req, res) => {
-    res.render(`buy/index`);
+  res.render(`buy/index`);
 });
 
 // write to RFID form
 app.get("/write", (req, res) => {
-    res.render(`write/index`);
+  res.render(`write/index`);
 });
 
 // write to RFID form
 app.get("/read", (req, res) => {
-    res.render(`read/index`);
+  res.render(`read/index`);
 });
 
 /* server side processes */
 // user sign in process
 app.post("/login", (req, res) => {
-    console.log(req);
+  console.log(req);
 });
 
 // buy process
@@ -51,24 +47,25 @@ app.post("/buy", (req, res) => {});
 
 // write to RFID
 app.post("/write", (req, res) => {
-    const cmd = require('node-cmd');
-    cmd.get(`python3 src/py/write.py ${req.body.level} ${req.body.nis} ${req.body.money}`, (data, err, stderr) => {
-        if (!err) console.log(data);
-        else console.log(err)
-    })
+  const cmd = require("node-cmd");
+  // prettier-ignore
+  cmd.get(`python3 src/py/write.py ${req.body.level} ${req.body.nis} ${req.body.money}`, (data, err, stderr) => {
+      if (!err) console.log(data);
+      else console.log(err);
+    });
 });
 
 // write to RFID form
 app.post("/read", (req, res, next) => {
-let data;
-    const py = new PythonShell('./src/py/read.py');
-    py.on('message', function(m) {
-	data = m;
-    });
-    py.end(e => {
-        if (e) throw e;
-	res.render('read/output', { output: JSON.parse(data) });
-    });
+  let data;
+  const py = new PythonShell("./src/py/read.py");
+  py.on("message", m => {
+    data = m;
+  });
+  py.end(e => {
+    if (e) throw e;
+    res.render("read/output", { output: JSON.parse(data) });
+  });
 });
 
 app.listen(3000, console.log("3000"));
