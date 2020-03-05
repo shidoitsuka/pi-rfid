005abc1ceeb5bc9a9a6c43fea08158f9dd0194f2
Brian <mail@standinshd.site>
2020-03-05 14:14:15 +0700

    modified
    - added config
    - added master (dashboard)
    - functional sign up & sign in
    - functional add & edit items
    - notice user to sign out first when they try to open sign in & sign up form while they are logged in
    - jquery is loaded on top of others

--- index.js
+++ index.js
@@ -2,23 +2,59 @@ const express = require("express");
 const hbs = require("hbs");
 const bodyParser = require("body-parser");
 const path = require("path");
-const { PythonShell } = require("python-shell");
+const {
+  PythonShell
+} = require("python-shell");
 const app = express();
+const Enmap = require("enmap");
+const db = new Enmap({
+  name: "data"
+});
+const {
+  port
+} = require("./config.js");
 
 app.set("views", path.join(__dirname, "src/views"));
 app.set("view engine", "hbs");
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({
+  extended: false
+}));
 
 /* user open the page */
 // index
 app.get("/", (req, res) => {
-  res.render(`index/index`);
+  // parse cookies & get value of "user"
+  // prettier-ignore
+  const seller = req.headers.cookie == "" ? "undefined" : req.headers.cookie.split("; ")[0].split("=")[1];
+  if (seller == "undefined") return res.render(`login/index`);
+  res.render("index/index");
 });
 
 // login form
 app.get("/login", (req, res) => {
-  res.render(`login/index`);
+  // parse cookies & get value of "user"
+  // prettier-ignore
+  const seller = req.headers.cookie == "" ? "undefined" : req.headers.cookie.split("; ")[0].split("=")[1];
+  if (seller == "undefined") return res.render(`login/index`);
+  res.render("index/index", {
+    toast: true,
+    message: "Silahkan logout terlebih dahulu."
+  });
+});
+
+// Signup form
+app.get("/signup", (req, res) => {
+  // parse cookies & get value of "user"
+  // prettier-ignore
+  const seller = req.headers.cookie == "" ? "undefined" : req.headers.cookie.split("; ")[0].split("=")[1];
+  // check if "user" is not defined
+  if (seller == "undefined") return res.render(`signup/index`);
+  // if defined, show message to logout
+  res.render("index/index", {
+    toast: true,
+    message: "Silahkan logout terlebih dahulu."
+  });
 });
 
 // seller's form
@@ -36,10 +72,95 @@ app.get("/read", (req, res) => {
   res.render(`read/index`);
 });
 
-/* server side processes */
+// write to RFID form
+app.get("/master", (req, res) => {
+  // parse cookies & get value of "user"
+  // prettier-ignore
+  const seller = req.headers.cookie == "" ? "undefined" : req.headers.cookie.split("; ")[0].split("=")[1];
+
+  if (seller == "undefined") return res.render(`login/index`);
+  const users = db.indexes;
+  const barang = db.get(seller, "barang");
+  res.render(`master/index`, {
+    users,
+    seller,
+    barang
+  });
+});
+
+// user sign out process
+app.get("/logout", (req, res) => {
+  // re defined "user" in cookie as undefined
+  res.cookie("user");
+  // redirect to login form
+  res.redirect("/login");
+});
+
+// DUMP
+app.get("/e", (req, res) => {
+  // let x = db.get("brian").barang.map((item) => {
+  //   return item.itemName;
+  // });
+  // console.log(db.get("brian"));
+  console.log(db.deleteAll());
+  res.end();
+});
+// !DUMP
+
+/* server side processes
+ * all post methods are stored bellow.
+ */
 // user sign in process
 app.post("/login", (req, res) => {
-  const { username, password } = req.body;
+  // get username and password
+  const {
+    username,
+    password
+  } = req.body;
+  // check if username doesn't exists or username and password doesn't match
+  if (!db.has(username) || db.get(username, "password") != password) {
+    // render login form and show notice
+    res.render("login/index", {
+      toast: true,
+      message: "User tidak ada atau sandi salah!",
+      class: "fail"
+    });
+  } else {
+    // if it matched set "user" cookie to username
+    res.cookie("user", username);
+    // render index
+    res.render("index/index");
+  }
+});
+
+// user sign up process
+app.post("/signup", (req, res) => {
+  // get username and password
+  const {
+    username,
+    password
+  } = req.body;
+  // check if username doesn't exists in db
+  if (!db.has(username)) {
+    // set a new username and password
+    db.set(username, {
+      password: password,
+      barang: {}
+    });
+    // re-render sign up and notify user
+    res.render(`signup/index`, {
+      toast: true,
+      message: "Berhasil!",
+      class: "success"
+    });
+  } else {
+    // if existed, notify user
+    res.render(`signup/index`, {
+      toast: true,
+      message: "Sudah Terdaftar!",
+      class: "fail"
+    });
+  }
 });
 
 // buy process
@@ -48,24 +169,77 @@ app.post("/buy", (req, res) => {});
 // write to RFID
 app.post("/write", (req, res) => {
   const cmd = require("node-cmd");
+  // our data
+  const {
+    level,
+    nis,
+    money
+  } = req.body;
+  // run write.py script
   // prettier-ignore
-  cmd.get(`python3 src/py/write.py ${req.body.level} ${req.body.nis} ${req.body.money}`, (data, err, stderr) => {
-    if (!err) res.render("write/failed", { error: err });
+  cmd.get(`python3 src/py/write.py ${level} ${nis} ${money}`, (data, err, stderr) => {
+    // if its NOT error, render failed with error code
+    if (!err) res.render("write/failed", {
+      error: err
+    });
+    // if its error, render success (idk whats going on)
     else res.render("write/success");
   });
 });
 
 // read RFID data
 app.post("/read", (req, res) => {
+  // temp variable
   let data;
+  // get read.py
   const py = new PythonShell("./src/py/read.py");
+  // check incoming output from python
   py.on("message", function(m) {
+    // store it in data
     data = m;
   });
+  // do this when python has finished its job
   py.end(err => {
-    if (err) res.render("error", { error: err });
-    res.render("read/output", { output: JSON.parse(data.replace(/'/g, '"')) });
+    // if error, render error with error data
+    if (err) res.render("error", {
+      error: err
+    });
+    // else, render card data
+    // prettier-ignore
+    else res.render("read/output", {
+      output: JSON.parse(data.replace(/'/g, '"'))
+    });
   });
 });
 
-app.listen(3000, "172.16.7.53", console.log("3000"));
+// add item to DB
+app.post("/addItem", (req, res) => {
+  const {
+    itemName,
+    itemPrice
+  } = req.body;
+  // prettier-ignore
+  const seller = req.headers.cookie == "" ? "undefined" : req.headers.cookie.split("; ")[0].split("=")[1];
+  db.set(seller, {
+    name: itemName,
+    price: itemPrice
+  }, `barang.${itemName}`);
+  res.redirect("/master");
+});
+
+// edit item info
+app.post("/editPrice", (req, res) => {
+  // get form data
+  const {
+    selectedItem,
+    newPrice
+  } = req.body;
+  const itemName = selectedItem.split("-")[0];
+  // parse cookies & get value of "user"
+  // prettier-ignore
+  const seller = req.headers.cookie == "" ? "undefined" : req.headers.cookie.split("; ")[0].split("=")[1];
+  db.set(seller, newPrice, `barang.${itemName}.price`);
+  res.redirect("/master");
+});
+
+app.listen(3000, port, console.log("3000"));
