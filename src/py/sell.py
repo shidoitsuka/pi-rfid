8f4f2aa92e7b9548102e2d48ddba08be196e45c4
Brian <mail@standinshd.site>
2020-03-10 07:52:02 +0700

    updated sell.py

--- src/py/sell.py
+++ src/py/sell.py
@@ -7,24 +7,24 @@ from mfrc522 import SimpleMFRC522
 reader = SimpleMFRC522()
 
 try:
-        id, data = reader.read()
-        try:
-            output = ast.literal_eval(data)
-            lvl = output["l"]
-            nis = output["n"]
-            uang = int(output["m"])
-            harga = 100
-            if uang < harga:
-                print("0")
-            else:
-                uang -= harga
-                data = {
-                    "l": lvl,
-                    "n": nis,
-                    "m": uang
-                }
-                reader.write( str(data) )
-        except:
+    id, data = reader.read()
+    try:
+        output = ast.literal_eval(data)
+        lvl = output["l"]
+        nis = output["n"]
+        uang = int(output["m"])
+        harga = 100
+        if uang < harga:
             print("0")
+        else:
+            uang -= harga
+            data = {
+                "l": lvl,
+                "n": nis,
+                "m": uang
+            }
+            reader.write( str(data) )
+    except:
+        print("0")
 finally:
-        GPIO.cleanup()
+    GPIO.cleanup()
